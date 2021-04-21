2cfc58eb14805b6eb717882fe668a7375ec088b8
jacobshinta <jacobshinta@me.com>
2021-04-21 08:58:57 -0400

    add Projects

---
+++ CodeFirstMVC/CodeFirstMVC/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentContextDB")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
